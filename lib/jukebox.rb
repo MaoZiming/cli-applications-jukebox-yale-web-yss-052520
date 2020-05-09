8a091632e31ced2e8010d735a65acfa9d0df17be
Ziming Mao <ziming.mao@yale.edu>
2020-05-09 17:33:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,55 @@
 # Add your code here
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+
+def help
+
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+def play
+  puts "Please enter a song name or number:"
+  response = gets.strip
+  count = 0
+  while count < songs.size do
+    if songs[count] == response || count == response.to_i
+      puts "Playing <song name>"
+      return true
+      break
+    end
+  end
+  puts "Invalid input, please try again"
+end
+
+def list
+  count = 0
+  while count < songs.size do
+    puts "#{count+1}. " + songs[count]
+    count += 1
+  end
+
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run
+
+end
